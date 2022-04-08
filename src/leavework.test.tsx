a9d550d073e99cc731b3c9eda84e1a1db78eef73
Said <s.tashmanov@mybuh.kz>
2022-04-08 15:22:04 +0600

    fix:

---
+++ src/leavework.test.tsx
@@ -0,0 +1,129 @@
+// import {ExampleComponent} from '.'
+import {objectGet, objectSet} from './core/Helpers'
+
+describe('testing functions objectGet/objectSet', () => {
+  test('objectSet set name[1].folder value 123', () => {
+    expect(
+      objectSet({
+        value: 123,
+        path: 'name[1].test.folder[0].value[0]',
+        obj: {},
+      }).name[1].test.folder[0].value[0],
+    ).toBe(123)
+  })
+  test('objectGet get name[1].folder and expect 123', () => {
+    expect(
+      objectGet({
+        path: 'name[1].folder[0].value',
+        obj: {
+          name: [undefined, {folder: [{value: 123}]}],
+        },
+      }),
+    ).toBe(123)
+  })
+})
+
+describe('testing functions objectGet/objectSet', () => {
+  test('objectSet set name[1].folder value 123', () => {
+    expect(
+      objectSet({
+        value: 123,
+        path: 'name[1].test.folder[0].value[0]',
+        obj: {},
+      }).name[1].test.folder[0].value[0],
+    ).toBe(123)
+  })
+  test('objectGet get name[1].folder and expect 123', () => {
+    expect(
+      objectGet({
+        path: 'name[1].folder[0].value',
+        obj: {
+          name: [undefined, {folder: [{value: 123}]}],
+        },
+      }),
+    ).toBe(123)
+  })
+  test('objectSet set name[1].folder value 123', () => {
+    expect(
+      objectSet({
+        value: 123,
+        path: 'name[1].test.folder[0].value[0]',
+        obj: {},
+      }).name[1].test.folder[0].value[0],
+    ).toBe(123)
+  })
+  test('objectGet get name[1].folder and expect 123', () => {
+    expect(
+      objectGet({
+        path: 'name[1].folder[0].value',
+        obj: {
+          name: [undefined, {folder: [{value: 123}]}],
+        },
+      }),
+    ).toBe(123)
+  })
+})
+
+describe('testing functions objectGet/objectSet', () => {
+  test('objectSet set name[1].folder value 123', () => {
+    expect(
+      objectSet({
+        value: 123,
+        path: 'name[1].test.folder[0].value[0]',
+        obj: {},
+      }).name[1].test.folder[0].value[0],
+    ).toBe(123)
+  })
+  test('objectGet get name[1].folder and expect 123', () => {
+    expect(
+      objectGet({
+        path: 'name[1].folder[0].value',
+        obj: {
+          name: [undefined, {folder: [{value: 123}]}],
+        },
+      }),
+    ).toBe(123)
+  })
+})
+
+describe('testing functions objectGet/objectSet', () => {
+  test('objectSet set name[1].folder value 123', () => {
+    expect(
+      objectSet({
+        value: 123,
+        path: 'name[1].test.folder[0].value[0]',
+        obj: {},
+      }).name[1].test.folder[0].value[0],
+    ).toBe(123)
+  })
+  test('objectGet get name[1].folder and expect 123', () => {
+    expect(
+      objectGet({
+        path: 'name[1].folder[0].value',
+        obj: {
+          name: [undefined, {folder: [{value: 123}]}],
+        },
+      }),
+    ).toBe(123)
+  })
+  test('objectSet set name[1].folder value 123', () => {
+    expect(
+      objectSet({
+        value: 123,
+        path: 'name[1].test.folder[0].value[0]',
+        obj: {},
+      }).name[1].test.folder[0].value[0],
+    ).toBe(123)
+  })
+  test('objectGet get name[1].folder and expect 123', () => {
+    expect(
+      objectGet({
+        path: 'name[1].folder[0].value',
+        obj: {
+          name: [undefined, {folder: [{value: 123}]}],
+        },
+      }),
+    ).toBe(123)
+  })
+})
+/* * 44444 55555 66666 * */
